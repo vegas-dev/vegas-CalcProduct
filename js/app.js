23c55454dcaaa882d3a690fe6c7bc67232af7a9f
Ruslan <84710696+halloween-px@users.noreply.github.com>
2021-07-12 08:17:17 +0300

    calc app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let $tariffs = document.getElementsByClassName('services-content--item'),
 	$tariffInner = document.getElementById('inner'),
 	newTariff = document.getElementById('newTariff'),
+	$addServices = document.getElementById('add-services'),
 	newServices = document.getElementById('newServices');
 
 let newTariffJson = newTariff.getAttribute('data-json'),
@@ -10,14 +11,42 @@ let newTariffJson = newTariff.getAttribute('data-json'),
 
 
 
+for (let i = 0 ; i < newServicesJsonParse.length ; i++ ) {
+	createServiceElement(newServicesJsonParse[i], $addServices );
+}
+
+for (let i = 0 ; i < newTariffJsonParse.length ; i++ ) {
+	createTariffElement (newTariffJsonParse[i], $tariffInner );
+}
 
 
 
+function createTariffElement (tariff, $tariffWrapperEl ) {
+	// создать DOM элемент
+	const $tariffDiv = document.createElement('div') ;
+	// добавить класс 
+	$tariffDiv.classList.add('services-content--item') ;
+	// вставить внутрь HTML с шаблонным литералом и динамическими переменными
+	$tariffDiv.innerHTML = `
+	<label class="label" for="tariff-${tariff.name}">
+		<input class="services-content--${tariff.name}" id="tariff-${tariff.name}" data-tariff-title="${tariff.title}" name="rate" type="radio" value="${tariff.price}">
+		<span >${tariff.title} ${tariff.price}р</span>
+	</label>
+	`;
+	// добавить новый DOM элемент внутрь DOM элемента враппера
+	$tariffWrapperEl.append($tariffDiv);
+}
 
-	for (let i = 1 ; i < newTariffJsonParse.length ; i++ ) {
-		let $tariffsClone = $tariffs[0].cloneNode(true);
-		$tariffsClone.querySelector('span').innerHTML = newTariffJsonParse[i].title + ' ' + newTariffJsonParse[i].price ;
-		$tariffs[0].parentElement.append($tariffsClone);
-	}
-
-
+function createServiceElement (service , $serviceWrapperEl) {
+	// создать DOM элемент
+	const $addServiceElement = document.createElement('label');
+	// добавить атрибут for 
+	$addServiceElement.setAttribute('for', `add-${service.name}`);
+	// вставить внутрь HTML с шаблонным литералом и динамическими переменными
+	$addServiceElement.innerHTML= `
+	${service.title}- <span>${service.price} руб.</span>
+	<input data-add-title="${service.dataTitle}" id="add-${service.name}" class="add-content--${service.name}" name="add" type="checkbox" value="${service.price}">
+	`;
+	// добавить новый DOM элемент внутрь DOM элемента враппера
+	$serviceWrapperEl.append($addServiceElement);
+}
