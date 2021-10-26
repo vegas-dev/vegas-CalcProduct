1b6f07bd8b4b7f83cb028963343f25ffb713a889
Ruslan <>
2021-10-26 17:55:58 +0300

    calc

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const cloneElement = (elementToCloneSelector, wrapperElSelector) => {
 	return el;
 };
 
-
 const setData = (data) => {
 	const {brief, functional} = data;
 
@@ -14,13 +13,11 @@ const setData = (data) => {
 		const el = cloneElement('.functional-check.clone', '.functional-area');
 		const {title, price} = item;
 
-
 		el.querySelector('.functional-check--title').innerText = title;
 		el.querySelector('.functional-check--price').innerText = price;
 		el.querySelector('.form-check-input').value = price;
 		el.querySelector('.form-check-input').id += index;
 		el.querySelector('.form-check-label').htmlFor += index;
-
 		index++;
 	}
 
@@ -28,50 +25,75 @@ const setData = (data) => {
 	el.querySelector('.brief-title').innerText = brief.title;
 	el.querySelector('.brief-price').innerText = brief.price;
 
+	const $input = document.querySelectorAll('input');
+
+	function getSelectedServices() {
+		let selectedServices = [];
+		let selectedSale = 0;
+		let sum = 0;
 
 
-	let $input = document.querySelectorAll('input');
-	let funcCheck = document.querySelector('.functional-area-check');
-	let funcCheckWrapper = document.querySelector('.functional-area-wrapper');
-	let funcItemClone = document.querySelector('.functional-item.clone'),
-		funcItem = funcItemClone.cloneNode(true);
+		$input.forEach(function ($el) {
+			if ($el.type === 'checkbox') {
+				if ($el.checked) {
+					selectedServices.push({
+						price: $el.value,
+						title: $el.parentElement.querySelector('.functional-check--title').innerText,
+					});
+				}
+			} else if ($el.type === 'number') {
+				if ($el.value !== '' && $el.value !== undefined) {
+					selectedSale = +$el.value;
+				}
+			}
 
-	funcCheckWrapper.innerHTML = '';
+		});
 
-	for (let i = 0; i < $input.length; i++) {
-		$input[i].addEventListener('click', () => {
-			let selectedServices = [];
+		console.log(selectedSale);
+		for ( let i = 0; i < selectedServices.length; i++) {
+			sum += parseInt(selectedServices[i].price);
+		}
 
-			$input.forEach(function($el) {
-				if ($el.type === 'checkbox') {
-					if ($el.checked) {
-						selectedServices.push({
-							price: $el.value,
-							title: $el.parentElement.querySelector('.functional-check--title').innerText,
-						});
-					}
+		let buttonSale  = document.querySelector('#btnGetDiscount');
+		buttonSale.addEventListener('click', function () {
+			console.log("buttonSale");
+		});
+
+		return {
+			selectedServices: selectedServices,
+			selectedSale: selectedSale,
+			sum: sum
+		};
+	}
+
+	function getElementForRemove() {
+		let elemetntsForRemove = document.querySelectorAll('.functional-area-check .functional-item:not(.clone)');  /*Ищем блок , потом его дочерний элемент и если не видим на нем класс clone удаляем ВСЕ кроме clone */
+		elemetntsForRemove.forEach(function (item) {
+			item.outerHTML = ''; /*вставляет вместе с оберткой */
+		});
+	}
+
+	function getElementCloneServices() {
+		for (let i = 0; i < $input.length; i++) {
+			$input[i].addEventListener('click', () => {
+				let selectedServices = getSelectedServices().selectedServices;
+
+				getElementForRemove();
+
+				for (let item of selectedServices) {
+					let el = cloneElement('.functional-item.clone', '.functional-area-check');
+					el.querySelector('.functional-item--title').innerText = item.title;
+					el.querySelector('.functional-item--price').innerText = item.price;
+					let totalPrice = document.querySelector('.total-price');
+					totalPrice.innerText = getSelectedServices().sum;
+
 				}
 			});
 
-			selectedServices.forEach(function (obj) {
-				funcItem.classList.remove('clone');
-				funcItem.querySelector('.functional-item--title').innerText = obj.title;
-				funcItem.querySelector('.functional-item--price').innerText = obj.price;
-
-				funcCheckWrapper.insertAdjacentHTML(funcItem);
-			});
- 
-
-			//.functional-area-check .functional-item:not(.clone)
-
-			/*for (let item of selectedServices){
-				let el = cloneElement('.functional-item.clone', '.functional-area-check');
-				el.querySelector('.functional-item--title').innerText = item.title;
-				el.querySelector('.functional-item--price').innerText = item.price;
-			}
-			console.log(selectedServices);*/
-		});
+		}
 	}
+
+	getElementCloneServices();
 };
 // wrapper
 {/* <div class="functional-area"></div> */
